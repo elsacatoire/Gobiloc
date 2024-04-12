432e5acea45b8e8c1f3d3f6c72880e95bccfe84c
Elsa Catoire <elsa.catoire@gmail.com>
2024-04-12 16:34:36 +0200

    ressort credentials

--- client/components/LoginCard.tsx
+++ client/components/LoginCard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState, FormEvent } from "react";
 
 import { Button } from "@/components/ui/button"
 import {
@@ -15,44 +15,70 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 export const LoginCard: React.FC = () => {
-    useEffect(() => {
 
-    }, []);
+    // État local pour suivre les valeurs des inputs
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleLogin = (e: React.FormEvent) => {
-        e.preventDefault(); // Empêche le formulaire de se soumettre normalement
-        // Récupérer les valeurs de l'e-mail et du mot de passe
-        const email = (document.getElementById('email') as HTMLInputElement).value;
-        const password = (document.getElementById('password') as HTMLInputElement).value;
-        console.log("Email:", email);
-        console.log("Mot de passe:", password);
-    }
+    // Gestion des changements dans les inputs
+    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    // Gestion de la soumission du formulaire
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const credentials = {
+            "email": email,
+            "password": password
+        }
+        console.log("credentials=>>", credentials);
+
+        // Réinitialisation des champs après la soumission
+        setEmail('');
+        setPassword('');
+    };
 
     return (
         <div>
             <Card className="w-[350px]">
-                <CardHeader>
-                    <CardTitle>Login</CardTitle>
-                    <CardDescription>Connecte toi pour profiter de GobiLoc !</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
+                <form onSubmit={handleSubmit} >
+                    <CardHeader>
+                        <CardTitle>Login</CardTitle>
+                        <CardDescription>Connecte toi pour profiter de GobiLoc !</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="user email">Email</Label>
-                                <Input type="email" id="name" placeholder="user@mail.com" />
+                                <Input
+                                    type="email"
+                                    id="userId"
+                                    value={email}
+                                    onChange={handleUsernameChange}
+                                    placeholder="user@mail.com" />
                             </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="password">Mot de passe</Label>
-                                <Input type="password" id="password" placeholder="mot de passe" />
+                                <Input
+                                    type="password"
+                                    id="password"
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                    placeholder="mot de passe" />
                             </div>
                         </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <Button variant="outline">Annuler</Button>
-                    <Button onClick={handleLogin} type="submit">Se connecter</Button>
-                </CardFooter>
+
+                    </CardContent>
+                    <CardFooter className="flex justify-between">
+                        <Button variant="outline">Annuler</Button>
+                        <Button type="submit">Se connecter</Button>
+                    </CardFooter>
+                </form>
             </Card>
         </div>
     )
