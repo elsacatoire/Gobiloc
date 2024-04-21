d0b17b24a2fce65dcdd6e7fef8d1672ab99b12ce
Elsa Catoire <elsa.catoire@gmail.com>
2024-04-21 12:07:16 +0200

    add ui components

---
+++ client/app/lists/page.tsx
@@ -0,0 +1,14 @@
+export default function Lists() {
+    return (
+        <div>
+            <div>
+                <h1>Hello, Next.js!</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+    )
+
+}
