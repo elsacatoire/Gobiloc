1106951558a640c822ff31578052fa89b8dd7df8
Elsa Catoire <elsa.catoire@gmail.com>
2024-08-16 16:35:05 +0200

    header : avatar varibale for img src

--- client/app/components/customsComponents/layout/Header.tsx
+++ client/app/components/customsComponents/layout/Header.tsx
@@ -9,6 +9,9 @@ interface HeaderProps {
 }
 
 export function Header({ title }: HeaderProps) {
+
+    const profileImgSrc = "/images/avatar3.jpg";
+
     return (
         <header className="fixed top-0 left-0 w-full shadow-md z-50">
             <div className="flex fixed w-screen justify-between items-center bg-gradient-to-r from-yellow-200 to-amber-400 text-gray-800 p-2 md:p-4">
@@ -18,7 +21,7 @@ export function Header({ title }: HeaderProps) {
                 <h1 className="absolute left-1/2 font-bold transform -translate-x-1/2 text-xl sm:text-2xl md:text-1xl">{title}</h1>
                 {title !== NavMenu.HOME ? (
                     <Link href='/userProfile'>
-                        <Avatar src="/images/avatar3.jpg" alt="Avatar" />
+                        <Avatar src={profileImgSrc} alt="Avatar" />
                     </Link>
                 ) : (
                     <div className="w-6"></div> // Espace réservé pour le bouton "Settings"
