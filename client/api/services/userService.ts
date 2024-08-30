c88f9d675708f9d93b774ed135d7a16f442a8ac0
Elsa Catoire <elsa.catoire@gmail.com>
2024-08-30 16:10:23 +0200

    feat: client/UserProfile service and fetch to data viz user

---
+++ client/api/services/userService.ts
@@ -0,0 +1,18 @@
+import { UserType } from "@/app/types/UserType";
+import axios from "axios";
+
+export const fetchCurrentUser = async () => {
+    try {
+        console.log("sertvice avant fetch");
+
+        const response = await axios.get("http://localhost:8000/api/user/");
+        console.log("response=>", response.data);
+
+        const todo: UserType[] = response.data;
+        return todo;
+    } catch (error) {
+        console.log("error servce");
+
+        throw new Error("Impossible de récupérer les données de l'utilisateur.");
+    }
+};
