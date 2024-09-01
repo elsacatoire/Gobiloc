9464380b5f41a7f4f567e6e3d1d6c8bd0fba1334
Elsa Catoire <elsa.catoire@gmail.com>
2024-09-01 15:19:28 +0200

    feat: client/oimplmant context for auth

--- client/context/AuthContext.js
+++ client/context/AuthContext.js
@@ -1,13 +1,16 @@
-import { createContext, useState, useEffect } from "react";
+'use client'
 
-const AuthContext = createContext();
+import { createContext, useState, useContext } from 'react';
 
-export default AuthContext;
+const AuthContext = createContext({ name: '' }); // Ajout d'une valeur par défaut
 
 export const AuthProvider = ({ children }) => {
+
     return (
-        <AuthContext.Provider value={{ "name": "elsa" }}>
+        <AuthContext.Provider value={{ name: 'elsa' }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
+
+export default AuthContext;
