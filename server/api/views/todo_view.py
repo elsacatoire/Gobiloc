ef66ce3cec11107367ab25abeb2f006389e10c22
TheUtopy <109255677+TheUtopy@users.noreply.github.com>
2024-09-20 11:28:39 +0200

    129 - removed a useless method in todo_view

--- server/api/views/todo_view.py
+++ server/api/views/todo_view.py
@@ -38,23 +38,6 @@ class TodoViewSet(ModelViewSet):
         flat = get_object_or_404(FlatShare, pk=flat_id)
         serializer.save(flat_share=flat)
 
-
-    # detail=False => act on the collection / True=> on a specific instance
-    # @action(detail=False, methods=['GET'], url_path='flat/(?P<flat_share_id>\d+)')
-    # def get_todos(self, request, flat_share_id=None):
-    #     """
-    #     Get all the to-do of a specific flat
-    #     """
-    #     if not flat_share_id:
-    #         return Response({"error": "Missing flat share id"}, status=status.HTTP_400_BAD_REQUEST)
-    #
-    #     # Use get_object_or_404 to check if the flat exists
-    #     get_object_or_404(FlatShare, pk=flat_share_id)
-    #
-    #     # If flat_share is found, proceed to get todos
-    #     todos = Todo.objects.filter(flat_share__id=flat_share_id).order_by("-updateDate")
-    #     return Response({"todos": todos.values()}, status=status.HTTP_200_OK)
-
     # @action(detail=True, methods=['GET'], url_path='tasks')
     # def get_todo_with_tasks(self, request, pk=None):
     #     """
