2a0bc0b82124141d2d4086bfceba5d641745c7d9
Elsa Catoire <elsa.catoire@gmail.com>
2024-09-27 11:13:04 +0200

    initial components to display budget

--- client/app/enums/NavMenuEnum.ts
+++ client/app/enums/NavMenuEnum.ts
@@ -1,8 +1,9 @@
 export enum NavMenu {
 	HOME = "Bocal",
-	CHECKLISTS = "Listes",
+	LISTS = "Listes",
 	AGENDA = "Agenda",
 	SAFE = "Trésor",
 	MESSAGES = "Bulles",
 	PROFIL = "Profil",
+    EXPENSE = "Budget"
 }
