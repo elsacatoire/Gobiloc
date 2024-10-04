7a973c0817f717f8bc7829615697a695b1643f77
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-04 12:20:33 +0200

    structure for feat budget with all files to handke simple crud on budget anbd expense + migration file

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from api.models import Category, FlatShare, Task, Todo, User
+from api.models import Category, FlatShare, Task, Todo, User, Budget, Expense
 
 admin.site.register(User)
 admin.site.register(FlatShare)
 admin.site.register(Todo)
 admin.site.register(Task)
 admin.site.register(Category)
+admin.site.register(Budget)
+admin.site.register(Expense)
---
+++ server/api/models/budget_model.py
@@ -0,0 +1,9 @@
+# api/models/budget_model.py
+
+from django.db.models import CASCADE, ForeignKey, Model, CharField
+from api.models import FlatShare
+
+
+class Budget(Model):
+    name = CharField(max_length=100)
+    flat_share = ForeignKey(FlatShare, on_delete=CASCADE)
