83f11abf098d0c04b27b26d90b77984146758894
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-04 12:45:48 +0200

    fix for lint

--- server/api/models/__init__.py
+++ server/api/models/__init__.py
@@ -5,5 +5,5 @@ from .flat_share_model import FlatShare as FlatShare
 from .task_model import Task as Task
 from .todo_model import Todo as Todo
 from .user_model import User as User
-from .budget_model import Budget
-from .expense_model import Expense
+from .budget_model import Budget as Budget
+from .expense_model import Expense as Expense
