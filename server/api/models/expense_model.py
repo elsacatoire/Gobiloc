b9124485fc35e53a02023ff6ebba31dbc8a50f07
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-11 10:18:49 +0200

    fix related names

--- server/api/models/expense_model.py
+++ server/api/models/expense_model.py
@@ -5,7 +5,7 @@ class Expense(Model):
     budget = ForeignKey("Budget", related_name="expenses", on_delete=CASCADE)
     description = TextField()
     amount = DecimalField(max_digits=10, decimal_places=2)
-    user = ForeignKey("User", related_name="user", on_delete=CASCADE)
+    user = ForeignKey("User", related_name="users", on_delete=CASCADE)
 
     def __str__(self):
         return f"{self.description} - {self.amount} €"
