f34292303c7292c731a51e82b4b7b4a157806034
TheUtopy <109255677+TheUtopy@users.noreply.github.com>
2024-10-11 11:02:08 +0200

    Fix flat_share/test_model duplication on field length

--- server/api/tests/flat_share/test_model.py
+++ server/api/tests/flat_share/test_model.py
@@ -20,16 +20,6 @@ class TestFlatShareModel(TestCase):
         with self.assertRaises(ValidationError):
             FlatShare(name='Test', description='a' * 256).full_clean()
 
-    def test_flat_share_name_max_length(self):
-        max_length = FlatShare._meta.get_field('name').max_length
-        with self.assertRaises(ValidationError):
-            FlatShare(name='a' * (max_length + 1), description='Test').full_clean()
-
-    def test_flat_share_description_max_length(self):
-        max_length = FlatShare._meta.get_field('description').max_length
-        with self.assertRaises(ValidationError):
-            FlatShare(name='Test', description='a' * (max_length + 1)).full_clean()
-
     def test_flat_share_creation_without_description(self):
         flat = FlatShare(name='Test Flat')
         flat.save()
