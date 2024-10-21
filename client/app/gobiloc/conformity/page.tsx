f72b24091618c47a0bb0d363a73369cf4b27d241
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-21 15:52:36 +0200

    conditionnal rendering for the header and add header to all new pages for UI harmony

--- client/app/gobiloc/conformity/page.tsx
+++ client/app/gobiloc/conformity/page.tsx
@@ -5,7 +5,7 @@ const ConformityPage = () => {
 	return (
 		<>
 			<Header title={"Conformité"} />
-			<div className="container mx-auto max-w-3xl pb-8">
+			<div className="container mx-auto p-4 max-w-2xl">
 				<h1 className="text-3xl font-bold mb-6">
 					Conformité aux Normes RGESN et RGAA
 				</h1>
