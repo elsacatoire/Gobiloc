62d38cba55517ed9f12574f131d63d4d43dc32e9
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-21 14:27:50 +0200

    create components to extract main links in conformity and description files

--- client/app/gobiloc/page.tsx
+++ client/app/gobiloc/page.tsx
@@ -1,16 +1,21 @@
-import Link from "next/link";
 import React from "react";
+import ContactLink from "../components/customsComponents/links/ContactLink";
+import RegisterLink from "../components/customsComponents/links/RegisterLink";
+import UseGobilocLink from "../components/customsComponents/links/UseGobilocLink";
+import UsefulLinks from "../components/customsComponents/links/UsefulLinks";
 
 const PresentationPage = () => {
 	return (
-		<div className="container mx-auto px-4 py-8">
-			<h1 className="text-4xl font-bold mb-6 text-center">
+		<div className="flex flex-col gap-8 md:gap-14 container mx-auto px-4 max-w-2xl">
+			<h1 className="text-2xl md:text-4xl font-bold text-center">
 				Bienvenue sur Gobiloc
 			</h1>
 
-			<section className="mb-8 text-center">
-				<h2 className="text-2xl font-semibold mb-4">Qu'est-ce que Gobiloc ?</h2>
-				<p className="mb-4">
+			<section className="flex flex-col gap-4 md:gap-8">
+				<h2 className="text-lg md:text-2xl font-semibold">
+					Qu'est-ce que Gobiloc ?
+				</h2>
+				<p>
 					Gobiloc est une application dédiée à la gestion collaborative pour les
 					colocataires. Elle vous permet de gérer vos tâches, suivre vos
 					dépenses partagées, organiser des événements et bien plus encore !
@@ -18,18 +23,22 @@ const PresentationPage = () => {
 					fonctionnalités conviviales qui rendent la vie en communauté plus
 					simple et organisée.
 				</p>
-				<p className="mb-4">
+				<p>
 					Notre objectif est de fournir un espace centralisé où les colocataires
 					peuvent gérer efficacement leurs tâches du quotidien, tout en
 					facilitant la communication et la collaboration entre eux.
 				</p>
 			</section>
 
-			<section className="mb-8 text-center">
-				<h2 className="text-2xl font-semibold mb-4">
+			<section className="flex flex-col gap-4 md:gap-8">
+				<h2 className="text-lg md:text-2xl font-semibold">
 					Fonctionnalités principales
 				</h2>
-				<ul className="list-disc list-inside mb-4">
+				<ul className="list-disc list-inside">
+					<li>
+						Gestion d'appartement. Créez et personnalisez votre espace de
+						colocation.
+					</li>
 					<li>
 						Création et gestion de listes partagées (courses, tâches ménagères,
 						etc.).
@@ -37,6 +46,15 @@ const PresentationPage = () => {
 					<li>
 						Suivi des dépenses communes et calcul automatique des balances.
 					</li>
+					<li>Visualisation de votre compte.</li>
+				</ul>
+			</section>
+
+			<section className="flex flex-col gap-4 md:gap-8">
+				<h2 className="text-lg md:text-2xl font-semibold">
+					Fonctionnalités à venir
+				</h2>
+				<ul className="list-disc list-inside">
 					<li>
 						Organisation d'événements avec synchronisation sur vos calendriers.
 					</li>
@@ -52,59 +70,12 @@ const PresentationPage = () => {
 				</p>
 			</section>
 
-			<section className="mb-8 text-center">
-				<h2 className="text-2xl font-semibold mb-4">Nous rejoindre</h2>
-				<p className="mb-4">
-					Prêt à rejoindre la communauté Gobiloc ? Créez un compte dès
-					maintenant pour découvrir toutes les fonctionnalités disponibles et
-					gérer votre colocation en toute simplicité.
-				</p>
-				<div className="flex justify-center space-x-4">
-					<Link
-						href="/register"
-						className="bg-teal-600 text-white px-4 py-2 rounded hover:bg-teal-700"
-					>
-						Créer un compte
-					</Link>
-					<Link
-						className="bg-orange-600 text-white px-4 py-2 rounded hover:bg-orange-700"
-						href="/login"
-					>
-						<span>Se connecter</span>
-					</Link>
-				</div>
-			</section>
-
-			<section className="mb-8 text-center">
-				<h2 className="text-2xl font-semibold mb-4">Liens utiles</h2>
-				<p className="mb-4">
-					Pour plus d'informations sur nos conditions d'utilisation et notre
-					politique de confidentialité, veuillez consulter les liens ci-dessous.
-				</p>
-				<div className="flex justify-center space-x-4">
-					<Link href="/terms" className="text-teal-600 hover:underline">
-						Conditions Générales d'Utilisation
-					</Link>
-					<Link href="/privacy" className="text-teal-600 hover:underline">
-						Politique de Confidentialité
-					</Link>
-				</div>
-			</section>
-
-			<section className="text-center">
-				<h2 className="text-2xl font-semibold mb-4">Contactez-nous</h2>
-				<p className="mb-4">
-					Si vous avez des questions ou des suggestions, n'hésitez pas à nous
-					contacter par email à
-					<a
-						href="mailto:contact@gobiloc.com"
-						className="text-teal-600 hover:underline ml-1"
-					>
-						contact@gobiloc.com
-					</a>
-					.
-				</p>
-			</section>
+			<div className="flex flex-col gap-4 md:gap-8">
+				<RegisterLink />
+				<UsefulLinks />
+				<UseGobilocLink />
+				<ContactLink />
+			</div>
 		</div>
 	);
 };
