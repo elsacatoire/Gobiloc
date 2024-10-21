db51e1c6e55369ca09fd5bd20d6feda1a4a5d8a7
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-21 12:34:32 +0200

    RGPD and corformity add links and pages

---
+++ client/app/gobiloc/terms/page.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const TermsAndConditions = () => {
+	return (
+		<div className="container mx-auto px-4 py-8">
+			<h1 className="text-3xl font-bold mb-4">
+				Conditions Générales d'Utilisation
+			</h1>
+
+			<p className="mb-4">
+				Les présentes conditions générales d’utilisation (les "Conditions")
+				régissent votre utilisation de l'application Gobiloc et de tous ses
+				services.
+			</p>
+
+			<h2 className="text-xl font-semibold mb-2">
+				1. Acceptation des Conditions
+			</h2>
+			<p className="mb-4">
+				En utilisant l'application Gobiloc, vous acceptez les présentes
+				Conditions. Si vous n'acceptez pas ces Conditions, vous ne devez pas
+				utiliser l'application.
+			</p>
+
+			<h2 className="text-xl font-semibold mb-2">2. Services Offerts</h2>
+			<p className="mb-4">
+				Gobiloc est une plateforme permettant aux colocataires de gérer leur
+				logement partagé, y compris la gestion des tâches, des budgets, et la
+				réservation d'événements.
+			</p>
+
+			<h2 className="text-xl font-semibold mb-2">3. Utilisation Acceptable</h2>
+			<p className="mb-4">
+				Vous vous engagez à utiliser Gobiloc conformément à la loi et aux bonnes
+				mœurs, et à ne pas utiliser l'application de manière à nuire à autrui ou
+				à enfreindre la loi.
+			</p>
+
+			<h2 className="text-xl font-semibold mb-2">
+				4. Propriété Intellectuelle
+			</h2>
+			<p className="mb-4">
+				Tous les contenus disponibles sur l'application Gobiloc (textes,
+				graphiques, logos, etc.) sont la propriété exclusive de Gobiloc ou de
+				ses partenaires. Toute reproduction est interdite sans autorisation
+				préalable.
+			</p>
+
+			<h2 className="text-xl font-semibold mb-2">5. Responsabilité</h2>
+			<p className="mb-4">
+				Nous déclinons toute responsabilité en cas de dysfonctionnement de
+				l'application ou de perte de données. L'utilisation de l'application se
+				fait à vos risques et périls.
+			</p>
+
+			<h2 className="text-xl font-semibold mb-2">
+				6. Modification des Conditions
+			</h2>
+			<p className="mb-4">
+				Nous nous réservons le droit de modifier les présentes Conditions à tout
+				moment. Les modifications seront publiées sur cette page, et votre
+				utilisation continue de l'application signifie que vous acceptez les
+				nouvelles Conditions.
+			</p>
+
+			<p className="mt-8">
+				Si vous avez des questions concernant ces conditions, veuillez nous
+				contacter à<strong> contact@gobiloc.com</strong>.
+			</p>
+		</div>
+	);
+};
+
+export default TermsAndConditions;
