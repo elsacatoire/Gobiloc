297ae13dcc93918cfbb560d16bda67ff4fb1a68b
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-21 14:56:35 +0200

    fix: hydration fix for register

---
+++ client/app/components/customsComponents/links/GobilocDescriptionLink.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const GobilocDescriptionLink = () => {
+	return (
+		<Link
+			href={"/gobiloc"}
+			className="text-orange-700 visited:text-orange-800 underline text-right"
+		>
+			En savoir plus sur Gobiloc
+		</Link>
+	);
+};
+
+export default GobilocDescriptionLink;
