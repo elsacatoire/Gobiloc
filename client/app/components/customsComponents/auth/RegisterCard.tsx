62237f8311f2d51a231388c9827d3f1d5b5bb82f
Elsa Catoire <elsa.catoire@gmail.com>
2024-10-21 14:56:35 +0200

    fix: hydration fix for register

--- client/app/components/customsComponents/auth/RegisterCard.tsx
+++ client/app/components/customsComponents/auth/RegisterCard.tsx
@@ -18,6 +18,7 @@ import { Input } from "@/app/components/ui/input";
 import { Label } from "@/app/components/ui/label";
 import Link from "next/link";
 import LogoFullName from "../design/LogoFullName";
+import GobilocDescriptionLink from "../links/GobilocDescriptionLink";
 
 export const RegisterCard: React.FC = () => {
 	const router = useRouter();
@@ -26,7 +27,7 @@ export const RegisterCard: React.FC = () => {
 	const [username, setUsername] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [consent, setConsent] = useState(false); // New state for consent
+	const [consent, setConsent] = useState(false);
 	const [error, setError] = useState<string | null>(null);
 
 	// Handeling input's changes
@@ -41,32 +42,32 @@ export const RegisterCard: React.FC = () => {
 	};
 
 	const handleConsentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setConsent(e.target.checked); // Update consent state
+		setConsent(e.target.checked);
 	};
 
 	// Handeling the login form submission
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		setError(null); // Reinit errors before submitting
+		setError(null);
 
 		const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 		if (!emailRegex.test(email)) {
 			setError("Veuillez entrer une adresse email valide.");
-			return; // Stop execution if email doesn't match criteria
+			return;
 		}
 		if (!passwordRegex.test(password)) {
 			setError(
 				"Le mot de passe doit contenir au moins 8 caractères, y compris des chiffres et des lettres.",
 			);
-			return; // Stop execution if password doesn't match criteria
+			return;
 		}
 		if (!consent) {
 			setError(
 				"Vous devez accepter les conditions générales et la politique de confidentialité.",
 			);
-			return; // Stop execution if consent is not given
+			return;
 		}
 
 		try {
@@ -81,49 +82,42 @@ export const RegisterCard: React.FC = () => {
 			router.push("/");
 		} catch (error) {
 			// Request errors
-			setError("Erreur lors de l'inscription. Veuillez réessayer.");
+			setError("Identifiants incorrects. Veuillez réessayer."); // TODO: Diplay erros on the page
 		}
 
 		// Reinit inputs
 		setEmail("");
 		setPassword("");
-		setConsent(false); // Reinit consent
+		setConsent(false);
 	};
 
 	return (
 		<div>
-			<Card className="w-svw p-2 md:max-w-96">
+			<Card className="w-[350px]">
 				<form onSubmit={handleSubmit}>
 					<CardHeader>
 						<div className="flex justify-center mb-5">
 							<LogoFullName />
 						</div>
 						<CardTitle>Ouvrir un compte</CardTitle>
-						<CardDescription className="text-teal-950">
+						<CardDescription className="text-black">
 							Enregistre toi pour rejoindre ta colloc et profiter des
 							fonctionnalités de gobiloc. Tu as déjà un compte ?
-							<div className="flex justify-between">
-								<Link
-									href="/login/user"
-									className="text-teal-700 visited:text-orange-700 underline"
-								>
-									{" "}
-									Se connecter
-								</Link>
-								<Link
-									href="/gobiloc"
-									className="text-teal-700 visited:text-orange-700 underline"
-								>
-									{" "}
-									Gobiloc, c'est quoi ?
-								</Link>
-							</div>
+							<Link
+								href="/login/user"
+								className="text-teal-700 visited:text-teal-700 underline font-semibold"
+							>
+								{" "}
+								Se connecter
+							</Link>
+							<br />
+							<GobilocDescriptionLink />
 						</CardDescription>
 					</CardHeader>
 					<CardContent>
 						<div className="grid w-full items-center gap-4">
-							<div className="flex flex-col space-y-1.5">
-								<Label htmlFor="user name">Pseudo</Label>
+							<div className="flex flex-col gap-2">
+								<Label htmlFor="user name">Nom</Label>
 								<Input
 									type="text"
 									id="username"
@@ -132,7 +126,7 @@ export const RegisterCard: React.FC = () => {
 									placeholder="Jane"
 								/>
 							</div>
-							<div className="flex flex-col space-y-1.5">
+							<div className="flex flex-col gap-2">
 								<Label htmlFor="user email">Email</Label>
 								<Input
 									type="email"
@@ -142,7 +136,7 @@ export const RegisterCard: React.FC = () => {
 									placeholder="user@mail.com"
 								/>
 							</div>
-							<div className="flex flex-col space-y-1.5">
+							<div className="flex flex-col gap-2">
 								<Label htmlFor="password">Mot de passe</Label>
 								<Input
 									type="password"
@@ -150,37 +144,33 @@ export const RegisterCard: React.FC = () => {
 									value={password}
 									onChange={handlePasswordChange}
 									placeholder="mot de passe"
-									className={`${error ? "bg-red-100 text-red-700" : ""}`} // Conditional class for error indication
+									className={`${error ? "bg-red-100 text-red-700" : ""}`}
 								/>
 								<span className="text-red-700 text-xs">{error}</span>
 							</div>
-
-							{/* Consent Checkbox */}
-							<div className="flex items-start space-x-2">
+							<div className="flex items-center space-x-2">
 								<input
 									type="checkbox"
 									id="consent"
 									checked={consent}
 									onChange={handleConsentChange}
-									className="h-4 w-4 rounded border-gray-300 text-teal-600 focus:ring-teal-500"
 								/>
-								<label htmlFor="consent" className="text-sm">
+								<Label htmlFor="consent">
 									J'accepte les{" "}
 									<Link
-										href="/gobiloc/terms"
-										className="underline text-teal-700"
+										href="/terms"
+										className="text-teal-700 visited:text-teal-800 underline"
 									>
 										conditions générales
 									</Link>{" "}
 									et la{" "}
 									<Link
-										href="gobiloc/privacy"
-										className="underline text-teal-700"
+										href="/privacy"
+										className="text-teal-700 visited:text-teal-800 underline"
 									>
 										politique de confidentialité
 									</Link>
-									.
-								</label>
+								</Label>
 							</div>
 						</div>
 					</CardContent>
